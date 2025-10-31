36ea5dc440f875a1844dce67603a09d790f9faaa
DataEngel <miguel.program.73@gmail.com>
2025-10-30 21:31:13 -0600

    test: stabilize API suite under anyio 3

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -1,17 +1,22 @@
+import os
 import unittest
 
-from fastapi.testclient import TestClient
 import importlib
-import os
-from challenge import app
+from fastapi.testclient import TestClient
 
 
 class TestBatchPipeline(unittest.TestCase):
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls) -> None:
+        super().setUpClass()
         os.environ["CHALLENGE_API_FAKE_MODEL"] = "1"
+        os.environ["ANYIO_BACKEND"] = "asyncio"
         importlib.reload(__import__("challenge.api.api"))
-        from challenge import app  # ensure fresh instance
-        self.client = TestClient(app)
+        from challenge import app
+        cls._app = app if app is not None else importlib.import_module("challenge.api.api").app
+
+    def setUp(self):
+        self.client = TestClient(self._app)
         
     def test_should_get_predict(self):
         data = {
